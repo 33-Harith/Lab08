88adda9075dfc2913f25e5823443712c9bf9b6b8
Harris1508 <HarrisFarris3@gmail.com>
2021-10-21 16:34:27 +0800

    Prototype done

---
+++ 33_harith_lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float Score;
+    public Text ScoreDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        ScoreDisplay.GetComponent<Text>().text = "Score :" + Score;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOverScene");
+
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Point")
+        {
+            Score += 1;
+        }
+    }
+}
---
+++ 33_harith_lab08/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObjectA;
+    public GameObject SpawnObjectB;
+    public GameObject SpawnObjectC;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjectsA", 1, 1);
+        InvokeRepeating("SpawnObjectsB", 2, 2);
+        InvokeRepeating("SpawnObjectsC", 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObjectsA()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObjectA, transform.position, transform.rotation);
+    }
+    void SpawnObjectsB()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObjectB, transform.position, transform.rotation);
+    }
+    void SpawnObjectsC()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObjectC, transform.position, transform.rotation);
+    }
+}
